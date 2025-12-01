73048337e3ca4af5162bb0adac3941779ab8bdf4
chang200601-ubs <chang200601@gmail.com>
2025-12-01 14:33:20 +0800

    v1_張伊傑

--- app/src/main/java/tw/edu/pu/csim/s1131633/s1131633/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131633/s1131633/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,10 +19,27 @@ import tw.edu.pu.csim.s1131633.s1131633.ui.theme.S1131633Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 強制螢幕為直式 (Portrait)
+        requestedOrientation = android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        // 隱藏狀態列 (Status Bar) 和 導航列 (Navigation Bar)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars())     // 隱藏上方狀態列
+        controller.hide(WindowInsetsCompat.Type.navigationBars()) // 隱藏下方導航列
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        // 啟用邊緣延伸效果
         enableEdgeToEdge()
+
+        // 設定 Compose 內容
         setContent {
             S1131633Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
